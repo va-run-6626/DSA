24b7ca2ed9e589288caced4a364248d55e8e9c6d
va-run-6626 <vbhutada143@gmail.com>
2024-10-11 16:26:00 +0530

    DFS problems

---
+++ src/Graphs/Prog8_01Matrix.java
@@ -0,0 +1,70 @@
+package Graphs;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Prog8_01Matrix {
+    public static void main(String[] args) {
+        int[][] mat = {
+                {0,0,0},
+                {0,1,0},
+                {1,1,1}
+        };
+        System.out.println("Question : ");
+        for(int[] i : mat){
+            System.out.println(Arrays.toString(i));
+        }
+        System.out.println();
+        System.out.println("Answer : ");
+        int[][] ans = solve(mat);
+        for(int[] i : ans){
+            System.out.println(Arrays.toString(i));
+        }
+    }
+
+    public static class DistanceInfo{
+        int x;
+        int y;
+        int dist;
+        public DistanceInfo(int x, int y, int dist){
+            this.x = x;
+            this.y = y;
+            this.dist = dist;
+        }
+    }
+
+    private static int[][] solve(int[][] mat) {
+        Queue<DistanceInfo> queue = new LinkedList<>();
+        boolean[][] vis = new boolean[mat.length][mat[0].length];
+        int[][] distance = new int[mat.length][mat[0].length];
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j <mat[0].length; j++){
+                if(mat[i][j] == 0){
+                    queue.add(new DistanceInfo(i,j,0));
+                    vis[i][j] = true;
+                }
+            }
+        }
+        int[] xDir = {-1,0,1,0};
+        int[] yDir = {0,1,0,-1};
+        while(!queue.isEmpty()){
+            DistanceInfo info = queue.poll();
+            int x = info.x;
+            int y = info.y;
+            int dist = info.dist;
+            distance[x][y] = dist;
+            for(int i= 0; i < 4; i++){
+                int r = x + xDir[i];
+                int c = y + yDir[i];
+
+                if(r >= 0 && r < mat.length && c >= 0 && c < mat[0].length && mat[r][c] == 1 && !vis[r][c]){
+                    queue.add(new DistanceInfo(r,c,dist+1));
+                    vis[r][c] = true;
+                }
+            }
+        }
+        return distance;
+    }
+
+}
---
+++ src/Graphs/Prog9_XO.java
@@ -0,0 +1,81 @@
+package Graphs;
+
+import java.util.Arrays;
+
+public class Prog9_XO {
+    public static void main(String[] args) {
+        char[][] grid= {
+                {'x','x','x','x','x'},
+                {'x','o','o','x','o'},
+                {'x','x','o','x','o'},
+                {'x','o','x','o','x'},
+                {'o','o','x','x','x'},
+        };
+        System.out.println("Given Matrix : ");
+        for(char[] a : grid){
+            System.out.println(Arrays.toString(a));
+        }
+        System.out.println();
+        solve(grid);
+        System.out.println("Flipped Matrix : ");
+        for(char[] a : grid) {
+            System.out.println(Arrays.toString(a));
+        }
+    }
+
+    private static void solve(char[][] grid) {
+        boolean[][] vis = new boolean[grid.length][grid[0].length];
+        int n = grid.length;
+        int m = grid[0].length;
+
+        //1st row
+        for(int j = 0; j < m ; j++){
+            if(!vis[0][j] && grid[0][j] == 'o'){
+                dfs(0,j,vis,grid);
+            }
+        }
+
+        //first col
+        for(int i = 0; i < n; i++){
+            if(!vis[i][0] && grid[i][0] == 'o'){
+                dfs(i,0,vis,grid);
+            }
+        }
+
+        // last row
+        for(int j = 0; j < m; j++){
+            if(!vis[n-1][j] && grid[n-1][j] == 'o'){
+                dfs(n-1,j,vis,grid);
+            }
+        }
+
+        //last col
+        for(int i = 0; i < n; i++){
+            if(!vis[i][m-1] && grid[i][m-1] == 'o'){
+                dfs(i,m-1,vis,grid);
+            }
+        }
+
+        // make changes
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 'o' && !vis[i][j]){
+                    grid[i][j] = 'x';
+                }
+            }
+        }
+    }
+
+    private static void dfs(int i, int j, boolean[][] vis, char[][] grid) {
+        vis[i][j] = true;
+        int[] xDir = {0,-1,0,1};
+        int[] yDir = {-1,0,1,0};
+        for(int k = 0; k < 4; k++){
+            int x = i + xDir[k];
+            int y = j + yDir[k];
+            if(x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] == 'o' && !vis[x][y]){
+                dfs(x,y,vis,grid);
+            }
+        }
+    }
+}
